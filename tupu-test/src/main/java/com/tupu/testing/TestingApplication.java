05d69b04be114efc65de5f8eb3710943185c53c6
tupu <p812816627@163.com>
2020-12-16 16:33:33 +0800

    first bean

---
+++ tupu-test/src/main/java/com/tupu/testing/TestingApplication.java
@@ -0,0 +1,20 @@
+package com.tupu.testing;
+
+import com.tupu.testing.dao.FirstBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author zhoupengxu
+ * @description
+ * @date 2020/12/16
+ **/
+
+public class TestingApplication {
+	public static void main(String[] args) {
+		ApplicationContext  context = new ClassPathXmlApplicationContext("spring-config.xml");
+		FirstBean firstBean = (FirstBean) context.getBean("firstBean");
+		String name = firstBean.getName();
+		System.out.println(name);
+	}
+}
---
+++ tupu-test/src/main/java/com/tupu/testing/dao/FirstBean.java
@@ -0,0 +1,17 @@
+package com.tupu.testing.dao;
+
+/**
+ * @author zhoupengxu
+ * @description
+ * @date 2020/12/16
+ **/
+public class FirstBean {
+	private String name;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
